f3757b5ace8255d47ac45b3c517d93507bfef89c
sathishmtech01 <sathish.mtech01@gmail.com>
2015-12-07 11:12:33 +0530

    Create spark_merging.scala
    input path  ==> arg1 -->  <path to file>/*/*.* (* indicates all files in the directory)
    output pah ==> arg2 -->  <path to store file>/

---
+++ spark_merging.scala
@@ -0,0 +1,15 @@
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.functions._
+
+object merging {
+def main(arg: Array[String]) {
+val sparkConf = new SparkConf().setAppName("aggregating")
+val sc = new SparkContext(sparkConf)
+val sqlContext = new SQLContext(sc)
+val input=arg(0)
+val output=arg(1)
+val data = sqlContext.read.json(input)
+data.repartition(1).save(output,"json")
+}
+}
